9799198929a612c580cbd25a09b930e50ed511bd
mariolo1985 <loserpunkx182@gmail.com>
2018-06-23 18:29:03 -0600

    landing a1 mod component

---
+++ src/components/LandingA1Mod/LandingA1Mod.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LandingA1Mod = () => {
+    return (
+        <div className='landing-mod-container'>
+            <div className='landing-text-wrapper'>
+                <div className='landing-text'>
+                    Solving Problems
+                </div>
+                <div className='landing-text'>
+                    <span className='landing-text-underline'>Designing</span> Solutions
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingA1Mod;
