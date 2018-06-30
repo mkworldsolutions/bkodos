7409717d1ed93754fdda8b3f4c143b513d9ca948
mariolo1985 <loserpunkx182@gmail.com>
2018-06-29 20:22:11 -0600

    updated to use request util

--- src/components/CaseStudies/CaseStudies.js
+++ src/components/CaseStudies/CaseStudies.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { httpRequest } from '../utils/httpRequest';
+
+import CaseStudyItem from './CaseStudyItem';
 
 class CaseStudies extends Component {
     constructor(props) {
@@ -9,22 +12,7 @@ class CaseStudies extends Component {
             caseStudyData: []
         };
         // get case json
-        const xReq = new XMLHttpRequest();
-        xReq.open("GET", "/js/caseStudies.json", true);
-        xReq.onload = () => {
-            if (xReq.readyState === 4) {
-                if (xReq.status === 200) {
-                    this.renderCaseStudies(xReq.responseText);
-                } else {
-                    console.log('status text: ', xReq.statusText);
-                }
-            }
-        };
-        xReq.onerror = () => {
-            console.log('error loading references: ', xReq.statusText);
-        }
-        xReq.send(null);
-
+        httpRequest('/js/caseStudies.json', this.renderCaseStudies);
     }
 
     renderCaseStudies = (caseJson) => {
@@ -33,12 +21,6 @@ class CaseStudies extends Component {
             caseLoaded: true,
             caseStudyData: caseStudies.cases
         });
-    }
-
-    getDotSvg = () => {
-        return (
-            <svg viewBox="0 0 512 512"><path fill='#0d75c1' d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z" /></svg>
-        );
     }
 
     render() {
@@ -65,20 +47,8 @@ class CaseStudies extends Component {
                                     {
                                         parentCase.Content.map((caseStudy, key) => {
                                             return (
-                                                <div className='case-study-item' key={key}>
-
-                                                    <div className='case-study-image-wrapper'>
-                                                        <a href={caseStudy.Url} className='case-study-link'>
-                                                            <img className='case-study-img' src={caseStudy.Image} alt={caseStudy.Name} />
-                                                        </a>
-                                                    </div>
-                                                    <div className='case-study-tagline clear'>
-                                                        <span className='case-study-name'>{caseStudy.Name}</span>
-                                                        <i className='case-study-tag-break'>{this.getDotSvg()}</i>
-                                                        <span className='case-study-type'>{caseStudy.Type}</span>
-                                                    </div>
-                                                </div>
-                                            )
+                                                <CaseStudyItem caseStudy={caseStudy} key={key} />
+                                            );
                                         })
                                     }
                                 </div>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
+import { httpRequest } from '../utils/httpRequest';
+
+import MenuItem from './MenuItem';
 
 class Menu extends Component {
-    constructor(props){
-        super(props);        
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menuLoaded: false,
+            menuData: {}
+        };
+
+        // get menu json
+        httpRequest("/js/menu.json", this.renderMenu);
     }
+
+    renderMenu = (menuJson) => {
+        const menu = JSON.parse(menuJson);
+        this.setState({
+            menuLoaded: menu.menu.length > 0,
+            menuData: menu
+        });
+    }
+
     render() {
+        const {
+            menuLoaded,
+            menuData
+        } = this.state;
+
+        if (!menuLoaded) {
+            return null;
+        }
+
+        const pathName = window.location.pathname;
         return (
             <div className='menu-container clear'>
                 <div className='logo-wrapper'>
-                    <a className='logo-link' href='#'>
-                        <img className='logo-img' src='/images/logo.jpg' alt='BK'/>
+                    <a className='logo-link' href='/'>
+                        <img className='logo-img' src='/images/logo.png' alt='BK' />
                     </a>
                 </div>
                 <div className='menu-wrapper'>
                     <ul className='menu clear'>
-                        <li className='menu-item'>
-                            <a className='menu-link' href='#'>
-                                Work
-                            </a>
-                        </li>
-                        <li className='menu-item'>
-                            <a className='menu-link' href='#'>
-                                About
-                            </a>
-                        </li>
-                        <li className='menu-item'>
-                            <a className='menu-link' href='#'>
-                                Contact
-                            </a>
-                        </li>
+                        {
+                            menuData.menu.map((menuItem, key) => {
+                                return (
+                                    <MenuItem key={key} menuItem={menuItem} pathName={pathName} />
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </div>
