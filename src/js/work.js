7679154bf2d276ad757cac545b8f283554a79fdc
mariolo1985 <loserpunkx182@gmail.com>
2018-07-29 19:02:42 -0600

    added navigation footer

--- src/js/work.js
+++ src/js/work.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import GoBackHeader from '../build/GoBackHeader/GoBackHeader';
+import NavigationHeader from '../build/NavigationHeader/NavigationHeader';
+import NavigationFooter from '../build/NavigationFooter/NavigationFooter';
 
 (() => {
     // render go back header
     const header = document.getElementById('goback-header');
     if (header != null) {
         render(
-            <GoBackHeader />,
+            <NavigationHeader />,
             header
+        );
+    }
+    // render footer navigation
+    const footerNav = document.getElementById('goback-container');
+    if (footerNav != null) {
+        render(
+            <NavigationFooter />,
+            footerNav
         );
     }
 })();
