3dafb1549c533c4c7aa3b84d2511f901d6c2d9d7
mariolo1985 <loserpunkx182@gmail.com>
2019-01-06 18:59:43 -0700

    cleaned up lint stuff and global component export file

--- src/components/LandingA1Mod/LandingA1Mod.js
+++ src/components/LandingA1Mod/LandingA1Mod.js
@@ -1,19 +1,13 @@
 import React from 'react';
 
-const LandingA1Mod = () => {
-    return (
-        <div className='landing-mod-container'>
-            <div className='landing-text-wrapper'>
-                <div className='landing-text'>
-                    Solving Problems
-                </div>
-                <div className='landing-text'>
-                    Designing Solutions
-                </div>
-                <span className='landing-text-underline'></span>
-            </div>
+const LandingA1Mod = () => (
+    <div className='landing-mod-container'>
+        <div className='landing-text-wrapper'>
+            <div className='landing-text'>Solving Problems</div>
+            <div className='landing-text'>Designing Solutions</div>
+            <span className='landing-text-underline' />
         </div>
-    )
-}
+    </div>
+);
 
 export default LandingA1Mod;
