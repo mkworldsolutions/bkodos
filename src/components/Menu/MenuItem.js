071c4db535042c3060663617e2143f64943c5a0c
mariolo1985 <loserpunkx182@gmail.com>
2019-01-06 22:02:29 -0700

    prop type checks

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MenuItem = ({
     menuItem,
     pathName
 }) => {
-
     const getMenuClass = () => {
         if (pathName === '/') {
             if (menuItem.url.indexOf('#') > -1) {
@@ -14,7 +14,8 @@ const MenuItem = ({
         const testPath = pathName.length > 1 ? pathName : 'zzz';
 
         return menuItem.url.indexOf(testPath) > -1 ? 'menu-link selected' : 'menu-link';
-    }
+    };
+
     return (
         <li className='menu-item'>
             <a className={getMenuClass()} href={menuItem.url}>
@@ -22,6 +23,11 @@ const MenuItem = ({
             </a>
         </li>
     );
-}
+};
+
+MenuItem.propTypes = {
+    menuItem: PropTypes.shape({}).isRequired,
+    pathName: PropTypes.string.isRequired
+};
 
 export default MenuItem;
